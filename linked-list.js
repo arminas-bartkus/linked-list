cdcd4afc0a1550e521fa7229db5b6ac45f33136e
arminas-bartkus <arminas.bartkus@gmail.com>
2025-09-29 23:25:42 +0100

    Add size, head, tail, at(index) functions

--- linked-list.js
+++ linked-list.js
@@ -25,6 +25,49 @@ class LinkedList {
     newNode.nextNode = this.head;
     this.head = newNode;
   }
+
+  size() {
+    let currentRef = this.head;
+    let counter = 1;
+    while (currentRef.nextNode) {
+      counter += 1;
+      currentRef = currentRef.nextNode;
+    }
+    return counter;
+  }
+
+  returnHead() {
+    return this.head;
+  }
+
+  returnTail() {
+    let currentRef = this.head;
+
+    while (currentRef.nextNode) {
+      currentRef = currentRef.nextNode;
+    }
+    return currentRef;
+  }
+
+  at(index) {
+    let currentSize = this.size();
+    if (index >= currentSize) {
+      return "Error";
+    }
+
+    let currentRef = this.head;
+    let indexNotMet = true;
+    let counter = 1;
+    while (indexNotMet) {
+      if (counter !== index + 1) {
+        currentRef = currentRef.nextNode;
+        counter += 1;
+      } else {
+        indexNotMet = !indexNotMet;
+      }
+    }
+    return currentRef;
+  }
 }
 
 class Node {
@@ -39,5 +82,4 @@ list.append("big");
 list.append("foo");
 list.prepend("second");
 list.prepend("first");
-
-console.log(list.head.nextNode.nextNode.nextNode.nextNode);
+console.log(list.at(4));
